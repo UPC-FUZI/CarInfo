48aebe78011d5217b4420dd7848f775144bc74eb
UPC_FUZI <1131653302@qq.com>
2018-04-27 14:48:12 +0800

    初始

---
+++ platform-web/src/main/java/com.junbo.platform/CarInfoApplication.java
@@ -0,0 +1,24 @@
+package com.junbo.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class CarInfoApplication extends WebMvcConfigureAdapter{
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarInfoApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
